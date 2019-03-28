2ee5997a522259e00c0aec77f22b1edae14460ca
Kristian Oksavik <you@example.com>
2019-03-28 14:49:06 +0100

    smol fix

---
+++ v.1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import random
+
+class Player(object):
+	
+	def __init__(self, name="spillernavn"):
+		self.name = spillernavn
+		self.energy = 0
+		self.hp = 10
+		self.points = 0
+		self.card = []
+    
+    def get_hp(self):
+        return self.hp
+
+    def get_energy(self) :
+        return_energy.
+    def get_points(self)
+        return_points
+
+player_one = player()
+
+print (player_one.get.hp)
+
+
+class Card(object)
+
+    def__init__(self):
+        self.name = None
+        self.description = None
+        self.cost = None
+        self.one_time_use = False
+        self.image = None
+
+
+
+cards = []
+Nuclearpowerplant = Card()
+Nuclearpowerplant.name = "Nuclearpowerplant"
+Nuclearpowerplant.description = " +2 points +3 heart "
+Nuclearpowerplant.cost = 6
+Nuclearpowerplant.one_time_use = True
+Nuclearpowerplant.image = None
+
+Energydrink = Card()
+Energydrink =  
+
+players = []
+for counter in range(4):
+    player = Player()
+    players.append(player)
+
+
+class die(object):
+    def __init__(self):
+    self.faces = [1,2,3,"smash", "heart", "energy"]
+
+    def trow.(self):
+        random.choice(self,faces)
+
+        return restult
+
+# opprett 6 terninger
+dice = []
+for counter in range(6)
+    die = Die()
+    dice.append(die)
+
+for player in players: 
